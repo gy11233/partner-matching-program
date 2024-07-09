f7d81e59a4b06e549130387734b03bb5e752d12c
gy11233 <19373234@buaa.edu.cn>
2024-07-09 14:19:28 +0800

    update src code

---
+++ src/main/java/com/gy11233/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.gy11233.mapper;
+
+import com.gy11233.model.domain.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 用户 Mapper
+ *
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
---
+++ src/main/java/com/gy11233/model/domain/request/UserLoginRequest.java
@@ -0,0 +1,29 @@
+package com.gy11233.model.domain.request;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 用户登录请求体
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ */
+@Data
+public class UserLoginRequest implements Serializable {
+
+    private static final long serialVersionUID = 3191241716373120793L;
+
+    /**
+     * 用户账号
+     */
+    private String userAccount;
+
+    /**
+     * 用户密码
+     */
+    private String userPassword;
+
+
+}
